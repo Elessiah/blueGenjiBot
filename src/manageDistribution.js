43b7a028d87acc350deee3e7e16f7bc1e73fef62
Firekiking84 <kgalaxie84@gmail.com>
2024-12-16 19:26:33 +0100

    Patch crash bot

--- src/manageDistribution.js
+++ src/manageDistribution.js
@@ -1,29 +1,34 @@
 const {EmbedBuilder} = require("discord.js");
 
 async function manageDistribution(message, client, bdd, channelId, services) {
-    let has_send = false;
-    for (const service of services) {
-        if (message.content.toLowerCase().includes(service.name)) {
-            has_send = true;
-            const targets = await bdd.get("ChannelPartnerService",
-                ["id_channel"],
-                {Service: "ChannelPartnerService.id_service = Service.id_service"},
-                {"Service.name": service.name});
-            for (const target of targets) {
-                if (target.id_channel === channelId) {
-                    continue;
+    try {
+        let has_send = false;
+        for (const service of services) {
+            if (message.content.toLowerCase().includes(service.name)) {
+                has_send = true;
+                const targets = await bdd.get("ChannelPartnerService",
+                    ["id_channel"],
+                    {Service: "ChannelPartnerService.id_service = Service.id_service"},
+                    {"Service.name": service.name});
+                for (const target of targets) {
+                    if (target.id_channel === channelId) {
+                        continue;
+                    }
+                    const channel = await client.channels.fetch(target.id_channel);
+                    const embed = new EmbedBuilder().setAuthor({
+                        name: message.author.username,
+                        iconURL: message.author.displayAvatarURL(),
+                    }).setDescription(message.content);
+                    await channel.send({embeds: [embed]});
                 }
-                const channel = await client.channels.fetch(target.id_channel);
-                const embed = new EmbedBuilder().setAuthor({
-                    name: message.author.username,
-                    iconURL: message.author.displayAvatarURL(),
-                }).setDescription(message.content);
-                await channel.send({embeds: [embed]});
             }
         }
-    }
-    if (has_send) {
-        await message.react("🛰️");
+        if (has_send) {
+            await message.react("🛰️");
+        }
+    } catch (err) {
+        const owner = await client.users.fetch(process.env.OWNER_ID);
+        await owner.send("manageDistribution: \n" + err);
     }
 }
 
