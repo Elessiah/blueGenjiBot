e5e8e8015413121084a6ff0529b046156794c7f2
Firekiking84 <kgalaxie84@gmail.com>
2025-03-28 14:25:30 +0100

    Fix des perms admins n'étant plus bypass par Elessiah et Bounty

--- src/check/checkPermissions.js
+++ src/check/checkPermissions.js
@@ -2,9 +2,11 @@ const sendLog = require("../safe/sendLog");
 
 async function checkPermissions(interaction) {
     try {
+        if (interaction.member.id === process.env.OWNER_ID || interaction.member.id === process.env.PRESIDENT)
+            return true;
         const member = interaction.guild.members.cache.get(interaction.user.id);
         const isAdmin = interaction.channel.permissionsFor(member).has();
-        if (isAdmin || interaction.member.id === process.env.OWNER_ID || interaction.member.id === process.env.PRESIDENT)
+        if (isAdmin)
             return true;
     } catch (e) {
         // await sendLog(interaction.client, "Error while checking permissions : " + e.message);
