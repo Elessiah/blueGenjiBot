3f0cff67aeaae83cafd67015176be9d2c760bd7d
Firekiking84 <kgalaxie84@gmail.com>
2025-04-21 05:44:06 +0200

    Correction commande de redémarrage.

--- src/commandsHandlers/restartBot.js
+++ src/commandsHandlers/restartBot.js
@@ -1,11 +1,16 @@
 const safeReply = require("../safe/safeReply");
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 async function restartBot(client, interaction) {
     const userTry = interaction.options.getString("password");
     if (process.env.PASSWORD === userTry) {
         await safeReply(interaction, "See you soon ! Restarting...");
-        exec('sh ../bot.sh');
+        const child = spawn('../bot.sh', {
+            detached: true,
+            stdio: 'ignore',
+        });
+
+        child.unref();
     } else {
         await safeReply(interaction, "Wrong password ! Are you sure you have right to do this ?");
     }
