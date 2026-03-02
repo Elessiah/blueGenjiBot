76ee0467b173056c3f5f313574a36498d6840fd1
Elessiah <kgalaxie84@gmail.com>
2026-03-02 16:53:40 +0100

    Fix pas de lien d'invit du serveur origine

--- src/utils/getInviteFromMessage.ts
+++ src/utils/getInviteFromMessage.ts
@@ -1,23 +1,29 @@
 import type {Client, Message, TextChannel} from "discord.js";
 import {PermissionsBitField} from "discord.js";
 import {getInviteFromChannel} from "./getInviteFromChannel.js";
+import {sendLog} from "@/safe/sendLog.js";
 
 async function getInviteFromMessage(client: Client,
                                     message: Message) {
     try {
-        if (message.channel.isTextBased())
+        if (!message.channel.isTextBased()) {
             return ("");
+        }
         const channel: TextChannel = message.channel as TextChannel;
-        if (!channel)
+        if (!channel) {
             return ("");
+        }
         const permissions = channel.permissionsFor(message.author);
-        if (!permissions)
-            return "";
-        if (permissions.has(PermissionsBitField.Flags.CreateInstantInvite))
+        if (!permissions) {
             return ("");
+        }
+        if (!permissions.has(PermissionsBitField.Flags.CreateInstantInvite)) {
+            return ("");
+        }
         return await getInviteFromChannel(channel);
     } catch (e) {
-        return "";
+        await sendLog(client, "(getInviteFromMessage)Erreur pour : " + message.content + "\nMessage d'erreur : \n" + (e as TypeError).message);
+        return ("");
     }
 }
 
